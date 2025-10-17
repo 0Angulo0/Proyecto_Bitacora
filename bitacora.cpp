62bed64fa525414dacef6b5cc6a0660c147c1e0d
0Angulo0 <angulosolisdaniela15@gmail.com>
2025-10-16 21:23:31 -0600

    Agregue el binary search para la busqueda del mes inicial y final del rango que busca el usuario. También se agregó una función print para poder imprimir este rango y el menu que se le va a desplegar al usuario para que pueda interactuar con el codigo.

--- bitacora.cpp
+++ bitacora.cpp
@@ -17,13 +17,15 @@ class info{
             this-> ip = ip;
             this-> falla = falla;
         }
+        string getmes() { return mes; };
+        int getdia() { return dia; };
         string design(){  //& titulo de la tabla
             return "FECHA |  HORA  |   DIRECCIÓN IP   | FALLA | \n-----------------------------------------";
         };
         void display(){  //& formato del texto
             cout << setw(3) << left << mes << " | " << setw(2) << left << dia << " | " << setw(8) << left << hora << " | " << setw(16) << left << ip << " |" << setw(40) << left << falla << endl;
         };
-        bool operator<(info &in){  //& sobrecarga del operador < 
+        bool operator<(info &in){  //& sobrecarga del operador < usado para el quick sort
             string meses[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};  //array con los meses
             int mesIzq = 0, mesDer = 0; //variables donde guardaremos el indice de los meses a comparar
             for (int i = 0; i < 12; i++){
@@ -39,12 +41,66 @@ class info{
         void save(vector <info> &b1){  //& creacion de un nuevo txt con el vector acomodado
             ofstream file("bitacoraAcomodada.txt"); //esccribe los datos al archivo
             file << b1[0].design() << endl;
-            for(int i = 1; i < b1.size() +1; i++){  //guarda cada objeto
+            for(int i = 0; i < b1.size(); i++){  //guarda cada objeto
                 file << b1[i].mes << " " << b1[i].dia << " " << b1[i].hora << " " << b1[i].ip << " " << b1[i].falla << "\n";
             };
             file.close();  //cierra el archivo ya guardado
             cout << "Cambios guardados con exíto :)" << endl;
-        };
+        }
+        int binarySearch_inicio(vector<info> &b1, string mesElegido, int diaElegido, int low, int high) {  //&busqueda binaria para la fecha de inicio
+            if (low > high) {  //caso donde termina la recursion
+                return low;
+            }
+            int mid = low + (high - low) / 2; // calcula el punto medio
+            string meses[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};  //array con los meses
+            int mesMid = 0, mesBuscar = 0; 
+            for (int i = 0; i < 12; i++) {  //mes a numero (con el index)
+                if (b1[mid].getmes() == meses[i]) { 
+                    mesMid = i; }
+                if (mesElegido == meses[i]) { 
+                    mesBuscar = i; }
+            }
+            //comparacion de meses y dias
+            if (mesMid > mesBuscar) {  //mes actual>al mes objetivo
+                return binarySearch_inicio(b1, mesElegido, diaElegido, low, mid - 1);  //busca a la izquierda
+            } else if (mesMid < mesBuscar) {  //mes actual < mes objetivo
+                return binarySearch_inicio(b1, mesElegido, diaElegido, mid + 1, high);  //busca a la derecha
+            } else {   //meses iguales, seguimos a comparar dias
+                if (b1[mid].getdia() >= diaElegido)   //dia es igual al del usuario o está en el lado izquierdo
+                return binarySearch_inicio(b1, mesElegido, diaElegido, low, mid - 1);
+                return binarySearch_inicio(b1, mesElegido, diaElegido, mid + 1, high);  //el dia esta en el lado derecho
+            }
+        }
+        int binarySearch_final(vector<info> &b1, string mesElegido, int diaElegido, int low, int high) { //&busqueda binaria para la fecha del final
+            if (low > high) {  //caso donde termina la recursion
+                return high;
+            }
+            int mid = low + (high - low) / 2; // calcula el punto medio
+            string meses[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};  //array con los meses
+            int mesMid = 0, mesBuscar = 0; 
+            for (int i = 0; i < 12; i++) {  //mes a numero (con el index)
+                if (b1[mid].getmes() == meses[i]) { 
+                    mesMid = i; }
+                if (mesElegido == meses[i]) { 
+                    mesBuscar = i; }
+            }
+            //comparacion de meses y dias
+            if (mesMid > mesBuscar) {  //mes actual>al mes objetivo
+                return binarySearch_final(b1, mesElegido, diaElegido, low, mid - 1);  //busca a la izquierda
+            } else if (mesMid < mesBuscar) {  //mes actual < mes objetivo
+                return binarySearch_final(b1, mesElegido, diaElegido, mid + 1, high);  //busca a la derecha
+            } else {   //meses iguales, seguimos a comparar dias
+                if (b1[mid].getdia() <= diaElegido)   //dia es igual al del usuario o está en el lado izquierdo
+                return binarySearch_final(b1, mesElegido, diaElegido, mid + 1, high);
+                return binarySearch_final(b1, mesElegido, diaElegido, low, mid - 1);  //el dia esta en el lado derecho
+            }
+        }
+        void print(vector <info> &b1, int inicio, int final){  //& imprime los valores en el rango encontrado
+            b1[0]. design();
+            for (int i = inicio; i < final + 1; i++){
+                b1[i].display();
+            }
+        }
 };
 int partition(vector <info> &b1, int low, int high){
     info pivot = b1[high];  //se establece el pivote central
@@ -89,18 +145,38 @@ int main(){
         values[4] = falla;    
         b1.push_back(info(values[0], stoi(values[1]), values[2], values[3], values[4])); //agregamos el objeto al vector      
     }    
+    file.close();
     cout << "Datos agregados al vector :)" << endl;   
 
     quick_sort(b1, 0, b1.size()-1); //acomoda los datos ingresados por fecha
     cout << "Datos acomodados con exito :)" << endl;
-    
-
-    b1[1].display();
-    b1[2].display();
-    file.close();
 
     b1[0].save(b1);  //guarda el vector acomodado en un nuevo archivo
-
+    
+    int diaInicial, diaFinal;  //variables usadas para respuestas del usuario
+    string mesInicial, mesFinal;
+    cout << endl;
+    cout << "Busqueda en la bítacora de errores" << endl;  //busqueda por rango
+    cout << "¿Cuál es el mes en el que quiere empezar la busqueda? Escriba las primeras 3 letras del mes en ingles: Jan, Feb, etc." << endl;
+    cin >> mesInicial;
+    cout << "¿Cuál es el día en el que quiere empezar la busqueda?" << endl;
+    cin >> diaInicial;
+    cout << "¿Cuál es el mes en el que quiere terminar la busqueda? Escriba las primeras 3 letras del mes en ingles: Jan, Feb, etc." << endl;
+    cin >> mesFinal;
+    cout << "¿Cuál es el día en el que quiere terminar la busqueda?" << endl;
+    cin >> diaFinal;
+    int inicio = b1[0].binarySearch_inicio(b1, mesInicial, diaInicial, 0, b1.size()-1);  //llama a la busqueda para la primera decha
+    int Final = b1[0].binarySearch_final(b1, mesFinal, diaFinal, 0, b1.size()-1);  //llama a la busqueda para la fecha final
+    
+    if (inicio > Final || inicio >= b1.size() || Final < 0) {
+    cout << "No se encontraron registros entre " << mesInicial << " " << diaInicial << " y " << mesFinal << " " << diaFinal << endl;
+    cout << "Por favor, revise que los datos ingresados fueron escritos de forma correcta." << endl;
+    } else {
+        cout << endl;
+        cout << "Resultados de la búsqueda: " << endl;
+        b1[0].print(b1, inicio, Final);//imprime el rango de datos encontrados
+        cout << "Valores encontrados: " << Final - inicio << endl;
+    }
 
     return 0;
 }
